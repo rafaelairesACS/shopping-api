4f66701ca5e16f4cf9bf983af7dd469ab926a60e
Rafael <rafaelaires.acs@gmail.com>
2023-06-06 11:19:58 -0300

    cap 11

---
+++ src/test/java/com/backend/java/shoppingapi/ProductServiceTest.java
@@ -0,0 +1,49 @@
+package com.backend.java.shoppingapi;
+
+import com.backend.java.shoppingapi.service.ProductService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.santana.java.back.end.dto.ProductDTO;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.io.IOException;
+
+public class ProductServiceTest {
+    public static MockWebServer mockBackEnd;
+    @InjectMocks
+    private ProductService productService;
+
+    @BeforeEach
+    void setUp() throws IOException {
+        mockBackEnd = new MockWebServer();
+        mockBackEnd.start();
+        String baseUrl = String.format("http://localhost:%s", mockBackEnd.getPort());
+        ReflectionTestUtils.setField(productService, "productApiURL", baseUrl);
+    }
+
+    @AfterEach
+    void tearDown() throws IOException {
+        mockBackEnd.shutdown();
+    }
+
+    @Test
+    void test_getProductByIdentifier() throws Exception {
+        ProductDTO productDTO = new ProductDTO();
+        productDTO.setPreco(1000F);
+        productDTO.setProductIdentifier("prod-identifier");
+        ObjectMapper objectMapper = new ObjectMapper();
+        mockBackEnd.enqueue(new MockResponse()
+                .setBody(objectMapper.writeValueAsString(productDTO))
+                .addHeader("Content-Type", "application/json"));
+        productDTO = productService.getProductByIdentifier("prod-identifier");
+                Assertions.assertEquals(1000F, productDTO.getPreco());
+        Assertions.assertEquals("prod-identifier", productDTO.getProductIdentifier());
+    }
+
+}
---
+++ src/test/java/com/backend/java/shoppingapi/ShopServiceTest.java
@@ -0,0 +1,55 @@
+package com.backend.java.shoppingapi;
+
+import com.backend.java.shoppingapi.dto.ItemDTO;
+import com.backend.java.shoppingapi.dto.ShopDTO;
+import com.backend.java.shoppingapi.model.Shop;
+import com.backend.java.shoppingapi.repository.ShopRepository;
+import com.backend.java.shoppingapi.service.ProductService;
+import com.backend.java.shoppingapi.service.ShopService;
+import com.backend.java.shoppingapi.service.UserService;
+import com.santana.java.back.end.dto.ProductDTO;
+import com.santana.java.back.end.dto.UserDTO;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+
+@ExtendWith(MockitoExtension.class)
+public class ShopServiceTest {
+    @InjectMocks
+    private ShopService shopService;
+    @Mock
+    private UserService userService;
+    @Mock
+    private ProductService productService;
+    @Mock
+    private ShopRepository shopRepository;
+
+    @Test
+    public void test_saveShop() {
+        ItemDTO itemDTO = new ItemDTO();
+        itemDTO.setProductIdentifier("123");
+        itemDTO.setPrice(100F);
+        ShopDTO shopDTO = new ShopDTO();
+        shopDTO.setUserIdentifier("123");
+        shopDTO.setItems(new ArrayList<>());
+        shopDTO.getItems().add(itemDTO);
+        shopDTO.setTotal(100F);
+        ProductDTO productDTO = new ProductDTO();
+        productDTO.setProductIdentifier("123");
+        productDTO.setPreco(100F);
+        Mockito.when(userService.getUserByCpf("123", "123")).thenReturn(new UserDTO());
+        Mockito.when(productService.getProductByIdentifier("123")).thenReturn(productDTO);
+        Mockito.when(shopRepository.save(Mockito.any())).thenReturn(Shop.convert(shopDTO));
+        shopDTO = shopService.save(shopDTO, "123");
+        Assertions.assertEquals(100F, shopDTO.getTotal());
+        Assertions.assertEquals(1, shopDTO.getItems().size());
+        Mockito.verify(shopRepository, Mockito.times(1)).save(Mockito.any());
+    }
+
+}
